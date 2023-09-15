3a7d5a43d4480adf8bedac7b20a9b5b7bc3e460d
Tani Umetsu <taniu@hawaii.edu>
2023-09-14 15:53:28 -1000

    Completed Problem Sets

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 class ViewController: UIViewController {
     //Global variables here Part 3
     var firstString = "The background color will turn blue"
-    var secondString = " The background color will turn green"
+    var secondString = "The background color will turn green"
+    let thirdString = 2
+    let fourthString = 1
+    
     
     
     @IBOutlet weak var firstLabel: UILabel!
     
+    @IBOutlet weak var Label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func changeColorButtonPressed(_ sender: Any) {
@@ -29,7 +36,35 @@ class ViewController: UIViewController {
             self.view.backgroundColor = UIColor.blue
             firstLabel.text = "This is cool"
             firstLabel.textColor = UIColor.black
-     }
+        }
     }
+    
+    @IBAction func textChangeButtonPressed(_ sender: Any) {
+        if firstString == "The background color will turn blue"{
+            Label1.text = "hi"
+            label2.text = "there"
+            
+        }
+        else
+        {
+            Label1.text = "there"
+            label2.text = "hi"
+        }
+        
+        
+    }
+    
+    @IBAction func additionButtonPressed(_ sender: Any) {
+        
+        if firstString.count > secondString.count {
+            print("The code in this block will not execute")
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.green
+            let fifthString = thirdString + fourthString
+            print(fifthString)
+        }
+    }
+    
 }
-
